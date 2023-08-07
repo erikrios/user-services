b98d8f65fa73a94064210495c39b431519dee6cc
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-08-07 10:59:39 +0700

    Add unit tests

---
+++ tests/user_mongo_repository_test.py
@@ -0,0 +1,67 @@
+import unittest
+from uuid import uuid4
+
+from models import User, Gender
+from user_mongo_repository import UserMongoRepository
+from repository_exception import NotFoundRepositoryException
+
+
+class MyTestCase(unittest.TestCase):
+    def test_insert(self):
+        user_to_insert = User(uuid4(), "Erik Rio Setiawan", 5, Gender.MALE)
+        repo = UserMongoRepository()
+        repo.insert(user_to_insert)
+        user = repo.get_by_id(user_to_insert.id)
+
+        self.assertEqual(str(user_to_insert.id), str(user.id))
+        self.assertEqual(user_to_insert.gender, user.gender)
+        self.assertEqual(user_to_insert.age, user.age)
+        self.assertEqual(user_to_insert.full_name, user.full_name)
+
+    def test_get_all(self):
+        user_to_insert = User(uuid4(), "Erik Rio Setiawan", 5, Gender.MALE)
+        repo = UserMongoRepository()
+        repo.insert(user_to_insert)
+        users = repo.get_all()
+
+        self.assertGreater(len(users), 0)
+
+    def test_get_by_id(self):
+        user_to_insert = User(uuid4(), "Erik Rio Setiawan", 5, Gender.MALE)
+        repo = UserMongoRepository()
+        repo.insert(user_to_insert)
+        user = repo.get_by_id(user_to_insert.id)
+
+        self.assertEqual(str(user_to_insert.id), str(user.id))
+        self.assertEqual(user_to_insert.gender, user.gender)
+        self.assertEqual(user_to_insert.age, user.age)
+        self.assertEqual(user_to_insert.full_name, user.full_name)
+
+    def test_update(self):
+        user_to_insert = User(uuid4(), "Erik Rio Setiawan", 5, Gender.MALE)
+        repo = UserMongoRepository()
+        repo.insert(user_to_insert)
+
+        user_to_insert.full_name = "Test New Name"
+
+        repo.update(user_to_insert.id, user_to_insert)
+
+        user = repo.get_by_id(user_to_insert.id)
+
+        self.assertEqual(str(user_to_insert.id), str(user.id))
+        self.assertEqual(user_to_insert.gender, user.gender)
+        self.assertEqual(user_to_insert.age, user.age)
+        self.assertEqual(user_to_insert.full_name, user.full_name)
+
+    def test_delete(self):
+        with self.assertRaises(NotFoundRepositoryException):
+            user_to_insert = User(uuid4(), "Erik Rio Setiawan", 5, Gender.MALE)
+            repo = UserMongoRepository()
+            repo.insert(user_to_insert)
+            repo.delete(user_to_insert.id)
+
+            repo.get_by_id(user_to_insert.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
